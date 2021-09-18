725e9dc022cada1a1c72ea6877d876b05e394f36
Martinus Ishoel <martinusishoel@live.no>
2021-09-18 14:11:46 +0200

    init

---
+++ .zshrc
@@ -0,0 +1,9 @@
+export ZSH=$HOME/.oh-my-zsh
+
+ZSH_THEME="simple"
+DISABLE_AUTO_UPDATE="true"
+DISABLE_UPDATE_PROMPT="true"
+
+plugins=(git zsh-autosuggestions ssh-agent asdf)
+
+source $ZSH/oh-my-zsh.sh
---
+++ setup.sh
@@ -0,0 +1,3 @@
+# Add ZSH plugins
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
